004ed2f0ef8de047573bcac9f70e53acf83b96ed
Cindy Cortes <cindy.indins@gmail.com>
2019-03-12 15:46:22 -0700

    retrieving the data in json

--- index.js
+++ index.js
@@ -14,7 +14,14 @@ app.get('/', function (req, res) {
 app.post("/", function (req, res) {
     // console.log(req.body.crypto);
     request("https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD", function (error, response, body) {
-        console.log(body)
+        // console.log(body)
+
+        var data = JSON.parse(body); // need to change this from json to js object
+        var price = data.last
+
+        res.send("<h1> The current price of bitcoin is " + price + " USD </h1>")
+        console.log(price);
+
     });
 });
 
