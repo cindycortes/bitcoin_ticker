b09fd15b8837bebd5da5c7c72303a1e0ee8abe99
Cindy Cortes <cindy.indins@gmail.com>
2019-03-12 16:30:32 -0700

    added a request for bitcoin conversion

--- index.js
+++ index.js
@@ -13,30 +13,41 @@ app.get('/', function (req, res) {
 
 app.post("/", function (req, res) {
     // console.log(req.body.crypto);
-    
-    var crypto = req.body.crypto; 
-    var fiat = req.body.fiat; 
-    
-    var baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/";
-    
-    var finalURL = baseURL + crypto + fiat;
-    
-    request(finalURL, function (error, response, body) {
+
+    var crypto = req.body.crypto;
+    var fiat = req.body.fiat;
+    var amount = req.body.amount;
+
+    var options = {
+        url: "https://apiv2.bitcoinaverage.com/convert/global",
+        method: "GET",
+        qs: {
+            from: crypto,
+            to: fiat,
+            amount: amount
+        }
+
+    }
+
+    request(options, function (error, response, body) {
         // console.log(body)
 
         var data = JSON.parse(body); // need to change this from json to js object
-        var price = data.last
+        var price = data.price;
 
-        var currentDate = data.display_timestamp;
-        
+        console.log(price);
+
+        var currentDate = data.time;
+
         res.write("<p>The current date is " + currentDate + "</p>");
-        res.write("<h1> The current price of " + crypto + " is " + price +  fiat + "</h1>")
+        res.write("<h1>" + amount + crypto + " is currently worth " + price + fiat + "</h1>")
 
         res.send()
         // console.log(price);
 
     });
 });
+
 
 
 app.listen(3000, function () {
